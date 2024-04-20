ca245569fbcf6f7a1e4d94beea59dadd98bc0720
gklelei <leleigideon97@gmail.com>
2024-04-20 21:49:34 +0300

    Working on restaurant Api

--- src/Controllers/MyRestaurantController.ts
+++ src/Controllers/MyRestaurantController.ts
@@ -6,6 +6,10 @@ import mongoose from "mongoose";
 const getMyRestaurant = async (req: Request, res: Response) => {
   try {
     const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found!" });
+    }
+    res.json(restaurant);
   } catch (error) {
     console.log("error", error);
     res.status(500).json({ message: "Error Fetching Restaurant" });
@@ -38,5 +42,6 @@ const createMyRestaurant = async (req: Request, res: Response) => {
   }
 };
 export default {
+  getMyRestaurant,
   createMyRestaurant,
 };
